eb2bc0a4fc98d37d27071e12794b835da3bf37e0
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-27 11:02:59 +0530

    lec 107 completed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
 
-const app = express();
+io.on('connection',(socket)=>{
+    console.log('New user connected.');
+
+    socket.on('disconnect',()=>{
+        console.log('one user disconnected.')
+    })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Node Chat App is started on Port:${port}`)
-})
+});
