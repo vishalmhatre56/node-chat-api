e3595b7432bc7dba24cbe1a65597c6cf1f8f4839
Vishal Mhatre <v.mhatre56@gmail.com>
2019-02-27 15:04:19 +0530

    lec 113 completed

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,23 @@ socket.on('connect', function () {
     console.log("Connected to server.")
 });
 
-socket.on('newMessage', function (message){
-    console.log(`%c${message.from}\n%c${message.text}\n%c-${message.createdAt}`,"color: green;font-size:14px","color: black","color: grey")
+socket.on('newMessage', function (message) {
+    console.log(message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`)
+    jQuery('#messages').append(li);
 })
 
 socket.on('disconnect', function () {
     console.log("Disconnected from server.")
 });
 
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: "user",
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    })
+})
--- server/server.js
+++ server/server.js
@@ -16,8 +16,9 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage("Admin", "Welcome to chat app."));
     socket.broadcast.emit('newMessage', generateMessage("Admin", "New user joined."));
 
-    socket.on('createMessage',(newMessage)=>{
+    socket.on('createMessage',(newMessage,callback)=>{
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+        callback('message from server')
     })
 
     socket.on('disconnect', () => {
