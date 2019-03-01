ad9991d828fd8027f6390767616eec8378d4f05b
Vishal Mhatre <v.mhatre56@gmail.com>
2019-03-01 10:36:11 +0530

    lec 116 completed

--- public/js/index.js
+++ public/js/index.js
@@ -40,12 +40,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location')
         alert('Unable to fetch location.');
     });
 });
