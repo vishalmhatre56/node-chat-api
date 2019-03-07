edfe5009a94f7300da78893c9733a68a1cb95eb7
Vishal Mhatre <v.mhatre56@gmail.com>
2019-03-07 18:59:54 +0530

    3rd edition lec 169 completed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,16 @@
 
 var socket = io();
 
+
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+
 socket.on('connect', function () {
     console.log("Connected to server.")
 });
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = jQuery('#message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         text: message.text,
         time: formattedTime,
         from: message.from
@@ -17,7 +20,7 @@ socket.on('newMessage', function (message) {
 socket.on('newLocationMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
     var template = jQuery('#location-message-template').html();
-    var html = Mustache.render(template,{
+    var html = Mustache.render(template, {
         from: message.from,
         time: formattedTime,
         url: message.url
@@ -32,12 +35,16 @@ socket.on('disconnect', function () {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     var messageInputField = jQuery('[name=message]');
-    socket.emit('createMessage', {
-        from: "User",
-        text: messageInputField.val()
-    }, function () {
-        messageInputField.val('');
-    });
+    if (messageInputField.val()) {
+        socket.emit('createMessage', {
+            from: "User",
+            text: messageInputField.val()
+        }, function () {
+            messageInputField.val('');
+        });
+    } else {
+        messageInputField.focus();
+    }
 });
 
 var locationButton = jQuery('#send-location');
@@ -47,11 +54,11 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by your browser.');
     }
 
-    locationButton.attr('disabled','disabled').text('Sending...');
+    locationButton.attr('disabled', 'disabled').text('Sending...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send Location');
-        socket.emit('createLocationMessage',{
+        socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
@@ -60,3 +67,5 @@ locationButton.on('click', function () {
         alert('Unable to fetch location.');
     });
 });
+
+socket.emit('join', { username, room })
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
-const {generateMessage,generateLocationMessage} = require('./uitils/message');
+const { generateMessage, generateLocationMessage } = require('./uitils/message');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -12,16 +12,21 @@ var io = socketIo(server);
 
 io.on('connection', (socket) => {
     console.log('New user connected.');
+    //methods: socket.emit, io.emit, socket.broadcast.emit, io.to().emit, socket.broadcast.to().emit.
+    socket.on('join', ({ username, room })=>{
+        socket.join(room)
 
-    socket.emit('newMessage', generateMessage("Admin", "Welcome to chat app."));
-    socket.broadcast.emit('newMessage', generateMessage("Admin", "New user joined."));
+        socket.emit('newMessage', generateMessage("Admin", "Welcome to chat app."));
+        socket.broadcast.to(room).emit('newMessage', generateMessage("Admin", `${username} has joined!`));
 
-    socket.on('createMessage',(newMessage,callback)=>{
+
+    })
+    socket.on('createMessage', (newMessage, callback) => {
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         callback('message from server')
     })
-    socket.on('createLocationMessage',(coords)=>{
-        io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude,coords.longitude))
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     })
     socket.on('disconnect', () => {
         socket.broadcast.emit('newMessage', generateMessage("Admin", "One user left."));
